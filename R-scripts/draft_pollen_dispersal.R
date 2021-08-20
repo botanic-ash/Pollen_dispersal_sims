26be3f3dd85b5b60b3951dffe7d592940ed6be5f
Kaylee Rosenberger <kayleejorose@gmail.com>
2021-08-20 12:30:32 -0600

    updating

--- R-scripts/draft_pollen_dispersal.R
+++ R-scripts/draft_pollen_dispersal.R
@@ -56,23 +56,73 @@ seeds_sampled = array(dim=c(trees_to_sample, seeds_to_sample))
 
 #number of pollen donors per maternal tree
 num_pollen_donors = 1 #defined as 1 for simplicity
-pollen_probability = c(1) #defined as 1 since there is 1 father per mother -- can change this as number of fathers increases, will be a proportion
+pollen_probability = c(1) #defined as 1 since there is 1 father per mother
+#can change this vector as number of fathers increases, will be proportions, eg., 0.2, 0.2, 0.6 (but should add up to 1!)
 
 #############################################################################################################
 #Defniing the function to create sets of seeds and sample them
 
-# move function to another script once complete with testing
-make_seed = function() {
+# move functions to another script once complete with testing
+
+#
+#make_seed()
+#args:
+#total_trees: total trees in population that have seeds
+#seeds_per_tree: number of seeds present on each tree (a simplification)
+#num_pollen_donors: the number of trees contributing pollen to a single mother tree
+
+#Function creates sets of seeds for all trees in the population
+#A seed is created by choosing a mother tree and a father tree
+#(or sets of father trees), and selecting alleles randomly 
+#from each parent for the seed to 'inherit'
+#If there are multiple father trees (pollen donors), then
+#The fathers are selected, and each one donates pollen (alleles)
+#to seeds based on a probability vector
+#Note: all trees are hermaphroditic and have the potential to self,
+#since trees are selected randomly with sample() 
+make_seed = function(total_trees, seeds_per_tree, num_pollen_donors) {
   for(i in 1:total_trees) {
     #choose mother -- a single row representing one maternal tree
+    #Ex.) x <- sample(total_trees, 1)
     #choose father(s) randomly -- a vector of rows representing potential pollen donors  
+    #Ex.) vec <- sample(total_trees, num_pollen_donors)
     for(j in 1:seeds_per_tree) {
       #choose maternal allele randomly -- need to sample() non-zero values only 
+      #Ex.) sample(gen_data[x,], 1) -- figure out how to filter data that is == 0
       #choose father based on probability vector 
+      #Ex.) y <- sample(vec, 1, prob = pollen_probability)
         #then choose allele from that father randomly -- need to sample() non-zero values only 
-      #save maternal allele + paternal allele as a seed
+        #Ex.) sample(gen_data[y,], 1)
+      #save the genotype of the seed (maternal allele + paternal allele)
       #EX.) seed_array[i,j] = list(maternal_allele, paternal_allele)
     }
   }
+  #return seed_array
+}
+#Note: currently we are using just 1 locus for simplicity (modeled in Simcoal)
+#it will be more difficult to implement multiple alleles 
+
+
+
+#
+#sample_seed()
+#args: 
+#trees_to_sample: number of trees collectors will sample seed from
+#seeds_to_sample: number of seeds collectors will sample from each tree
+
+#Function samples seeds from the array created in the function above
+#This represents collectors sampling seeds from trees within the population
+sample_seed = function(trees_to_sample, seeds_to_sample) {
+  for(i in 1:trees_to_sample) { #for each tree...
+    #sample a row from seed_array -- corresponding to a mother
+    #Ex.) sample(total_trees, 1) -> row 25
+    for(j in 1:seeds_to_sample) { #sample this many seeds
+      #sample a 'seed' from the row
+      #Ex.) sample(seeds_per_tree, 1) -> column 60
+      #save the seed's genotype to seeds_sampled array
+      #Ex.) seeds_sampled[25,60] = seed_array[25,60]
+    }
+  }
+  #return seeds_sampled
 }
 
